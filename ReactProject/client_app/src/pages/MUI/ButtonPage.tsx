d7febd2de8cb1546935c1e0fb89d4731e148de77
jinseok.park <jinseok1306@seetec.com>
2022-05-17 16:08:52 +0900

    테스트

---
+++ ReactProject/client_app/src/pages/MUI/ButtonPage.tsx
@@ -0,0 +1,17 @@
+﻿import React, { useCallback, useState } from "react";
+import Menu from '../../components/Menu';
+import Grid from '@mui/material/Grid';
+import BasicButtons from '../../components/Button/BasicButtons';
+
+const ButtonPage = () => {
+    return (
+        <div>
+            <Menu />
+            <div style={{ margin: 20 }}>
+                ㅇㅇㅇ 
+            </div>
+        </div>
+    );
+};
+
+export default ButtonPage;
