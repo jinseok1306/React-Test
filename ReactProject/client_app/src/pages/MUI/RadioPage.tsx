82a51d69785cc126092022895773981ffcdd8ea9
jinseok.park <jinseok1306@seetec.com>
2022-05-17 17:33:46 +0900

    MUI Error Radio 테스트 완료

--- ReactProject/client_app/src/pages/MUI/RadioPage.tsx
+++ ReactProject/client_app/src/pages/MUI/RadioPage.tsx
@@ -3,6 +3,7 @@ import Menu from '../../components/Menu';
 import Grid from '@mui/material/Grid';
 import RadioButtonsGroup from '../../components/Radio/RadioButtonsGroup';
 import RowRadioButtonsGroup from '../../components/Radio/RowRadioButtonsGroup';
+import ErrorRadios from '../../components/Radio/ErrorRadios';
 
 const RadioPage = () => {
     return (
@@ -17,6 +18,10 @@ const RadioPage = () => {
                     <Grid item xs={3}>
                         <b>RowRadioButtonsGroup</b><div>&nbsp;</div>
                         <RowRadioButtonsGroup />
+                    </Grid>
+                    <Grid item xs={3}>
+                        <b>ErrorRadios</b><div>&nbsp;</div>
+                        <ErrorRadios />
                     </Grid>
                 </Grid>
             </div>
