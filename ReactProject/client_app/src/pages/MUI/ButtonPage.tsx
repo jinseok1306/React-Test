986ad391531b97efd5e4fa05a299aafd026deaee
jinseok.park <jinseok1306@seetec.com>
2022-05-17 16:22:18 +0900

    MUI OutlinedButton 테스트 완료

--- ReactProject/client_app/src/pages/MUI/ButtonPage.tsx
+++ ReactProject/client_app/src/pages/MUI/ButtonPage.tsx
@@ -3,6 +3,7 @@ import Menu from '../../components/Menu';
 import Grid from '@mui/material/Grid';
 import BasicButtons from '../../components/Button/BasicButtons';
 import TextButtons from '../../components/Button/TextButtons';
+import OutlinedButtons from '../../components/Button/OutlinedButtons';
 
 const ButtonPage = () => {
     return (
@@ -11,12 +12,16 @@ const ButtonPage = () => {
             <div style={{ margin: 20 }}>
                 <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
                     <Grid item xs={3}>
-                        <b>BasicButtons</b>
+                        <b>Basic Buttons</b>
                         <BasicButtons />
                     </Grid>
                     <Grid item xs={3}>
-                        <b>TextButtons</b>
+                        <b>Text Buttons</b>
                         <TextButtons />
+                    </Grid>
+                    <Grid item xs={3}>
+                        <b>Outlined Buttons</b>
+                        <OutlinedButtons />
                     </Grid>
                 </Grid>
             </div>
