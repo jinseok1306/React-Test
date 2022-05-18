cfd402dfebe3992ae0b4eb78399e216cb19edea1
jinseok.park <jinseok1306@seetec.com>
2022-05-18 16:28:58 +0900

    MUI RagerSlider 테스트 완료

--- ReactProject/client_app/src/pages/MUI/SliderPage.tsx
+++ ReactProject/client_app/src/pages/MUI/SliderPage.tsx
@@ -4,6 +4,7 @@ import Grid from '@mui/material/Grid';
 import ContinuousSlider from '../../components/Slider/ContinuousSlider';
 import DiscreteSlider from '../../components/Slider/DiscreteSlider';
 import DiscreteSliderValues from '../../components/Slider/DiscreteSliderValues';
+import RangeSlider from '../../components/Slider/RangeSlider';
 
 const SliderPage = () => {
     return (
@@ -22,6 +23,10 @@ const SliderPage = () => {
                     <Grid item xs={3}>
                         <b>DiscreteSliderValues</b>
                         <DiscreteSliderValues />
+                    </Grid>
+                    <Grid item xs={3}>
+                        <b>RangeSlider</b>
+                        <RangeSlider />
                     </Grid>
                 </Grid>
             </div>
