e59e3f53384ce5218f12c706622a9882164e9783
jinseok.park <jinseok1306@seetec.com>
2022-05-19 11:30:45 +0900

    MUI DeletableChip 테스트 완료

--- ReactProject/client_app/src/pages/MUI/ChipPage.tsx
+++ ReactProject/client_app/src/pages/MUI/ChipPage.tsx
@@ -2,6 +2,7 @@
 import Menu from '../../components/Menu';
 import Grid from '@mui/material/Grid';
 import BasicChips from '../../components/Chip/BasicChips';
+import DeletableChips from '../../components/Chip/DeletableChips';
 
 const ChipPage = () => {
     return (
@@ -12,6 +13,10 @@ const ChipPage = () => {
                     <Grid item xs={3}>
                         <b>BasicChips</b><div>&nbsp;</div>
                         <BasicChips />
+                    </Grid>
+                    <Grid item xs={3}>
+                        <b>DeletableChips</b><div>&nbsp;</div>
+                        <DeletableChips />
                     </Grid>
                 </Grid>
             </div>
