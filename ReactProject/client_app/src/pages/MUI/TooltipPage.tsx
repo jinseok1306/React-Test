90ed3e466b17e34ba6491de16772957aa00159f7
jinseok.park <jinseok1306@seetec.com>
2022-05-19 14:35:58 +0900

    MUI ArrowTooltips 테스트 완료

--- ReactProject/client_app/src/pages/MUI/TooltipPage.tsx
+++ ReactProject/client_app/src/pages/MUI/TooltipPage.tsx
@@ -2,6 +2,7 @@
 import Menu from '../../components/Menu';
 import Grid from '@mui/material/Grid';
 import BasicTooltip from '../../components/Tooltip/BasicTooltip';
+import ArrowTooltips from '../../components/Tooltip/ArrowTooltips';
 
 const TooltipPage = () => {
     return (
@@ -9,10 +10,14 @@ const TooltipPage = () => {
             <Menu />
             <div style={{ margin: 20 }}>
                 <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
-                    <Grid item xs={6}>
+                    <Grid item xs={3}>
                         <b>BasicTooltip</b><div>&nbsp;</div>
                         <BasicTooltip />
-                    </Grid>                    
+                    </Grid>
+                    <Grid item xs={3}>
+                        <b>ArrowTooltips</b><div>&nbsp;</div>
+                        <ArrowTooltips />
+                    </Grid>
                 </Grid>
             </div>
         </div>
