f778a1778497154b7caf28b9a03be0ca23d738ff
jinseok.park <jinseok1306@seetec.com>
2022-05-19 13:32:08 +0900

    MUI NestedList 테스트 완료

--- ReactProject/client_app/src/pages/MUI/ListPage.tsx
+++ ReactProject/client_app/src/pages/MUI/ListPage.tsx
@@ -2,6 +2,7 @@
 import Menu from '../../components/Menu';
 import Grid from '@mui/material/Grid';
 import BasicList from '../../components/List/BasicList';
+import NestedList from '../../components/List/NestedList';
 
 const ListPage = () => {
     return (
@@ -12,7 +13,11 @@ const ListPage = () => {
                     <Grid item xs={3}>
                         <b>BasicList</b><div>&nbsp;</div>
                         <BasicList />
-                    </Grid>                    
+                    </Grid>
+                    <Grid item xs={3}>
+                        <b>NestedList</b><div>&nbsp;</div>
+                        <NestedList />
+                    </Grid>
                 </Grid>
             </div>
         </div>
