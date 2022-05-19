dcbe1548a44dd061c345ca4192d56b7f2fa42f74
jinseok.park <jinseok1306@seetec.com>
2022-05-19 14:12:57 +0900

    MUI DenseTable 테스트 완료

--- ReactProject/client_app/src/pages/MUI/TablePage.tsx
+++ ReactProject/client_app/src/pages/MUI/TablePage.tsx
@@ -3,6 +3,7 @@ import Menu from '../../components/Menu';
 import Grid from '@mui/material/Grid';
 import BasicTable from '../../components/Table/BasicTable';
 import DataTable from '../../components/Table/DataTable';
+import DenseTable from '../../components/Table/DenseTable';
 
 const TablePage = () => {
     return (
@@ -17,6 +18,10 @@ const TablePage = () => {
                     <Grid item xs={6}>
                         <b>DataTable</b><div>&nbsp;</div>
                         <DataTable />
+                    </Grid>
+                    <Grid item xs={6}>
+                        <b>DenseTable</b><div>&nbsp;</div>
+                        <DenseTable />
                     </Grid>
                 </Grid>
             </div>
