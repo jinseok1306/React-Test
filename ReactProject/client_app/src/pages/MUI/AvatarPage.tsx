c6139fd1a2eadc429b5dbecdbd1e71b635f63480
jinseok.park <jinseok1306@seetec.com>
2022-05-19 10:56:47 +0900

    MUI LetterAvatar 테스트 완료

--- ReactProject/client_app/src/pages/MUI/AvatarPage.tsx
+++ ReactProject/client_app/src/pages/MUI/AvatarPage.tsx
@@ -2,6 +2,7 @@
 import Menu from '../../components/Menu';
 import Grid from '@mui/material/Grid';
 import ImageAvatars from '../../components/Avatar/ImageAvatars';
+import LetterAvatars from '../../components/Avatar/LetterAvatars';
 
 const AvatarPage = () => {
     return (
@@ -12,7 +13,11 @@ const AvatarPage = () => {
                     <Grid item xs={3}>
                         <b>ImageAvatars</b><div>&nbsp;</div>
                         <ImageAvatars />
-                    </Grid>                    
+                    </Grid>
+                    <Grid item xs={3}>
+                        <b>LetterAvatars</b><div>&nbsp;</div>
+                        <LetterAvatars />
+                    </Grid>
                 </Grid>
             </div>
         </div>
