94dc694fdc81aa60ade8af98e18ee0768e51b569
jinseok.park <jinseok1306@seetec.com>
2022-05-19 16:23:02 +0900

    MUI CircularDeterminate 테스트 완료

--- ReactProject/client_app/src/pages/MUI/ProgressPage.tsx
+++ ReactProject/client_app/src/pages/MUI/ProgressPage.tsx
@@ -2,6 +2,7 @@
 import Menu from '../../components/Menu';
 import Grid from '@mui/material/Grid';
 import CircularIndeterminate from '../../components/Progress/CircularIndeterminate';
+import CircularDeterminate from '../../components/Progress/CircularDeterminate';
 
 const ProgressPage = () => {
     return (
@@ -12,7 +13,11 @@ const ProgressPage = () => {
                     <Grid item xs={3}>
                         <b>CircularIndeterminate</b><div>&nbsp;</div>
                         <CircularIndeterminate />
-                    </Grid>                   
+                    </Grid>
+                    <Grid item xs={3}>
+                        <b>CircularDeterminate</b><div>&nbsp;</div>
+                        <CircularDeterminate />
+                    </Grid>
                 </Grid>
             </div>
         </div>
