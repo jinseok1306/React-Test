fc4496685d38579192cafcf08c1623a72a2526e4
jinseok.park <jinseok1306@seetec.com>
2022-05-23 16:15:33 +0900

    MUI ScrollableTabsButtonAuto 테스트 완료

--- ReactProject/client_app/src/pages/MUI/TabsPage.tsx
+++ ReactProject/client_app/src/pages/MUI/TabsPage.tsx
@@ -3,6 +3,7 @@ import Menu from '../../components/Menu';
 import Grid from '@mui/material/Grid';
 import BasicTabs from '../../components/Tabs/BasicTabs';
 import FullWidthTabs from '../../components/Tabs/FullWidthTabs';
+import ScrollableTabsButtonAuto from '../../components/Tabs/ScrollableTabsButtonAuto';
 
 const TabsPage = () => {
     return (
@@ -10,13 +11,17 @@ const TabsPage = () => {
             <Menu />
             <div style={{ margin: 20 }}>
                 <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
-                    <Grid item xs={3}>
+                    <Grid item xs={4}>
                         <b>BasicTabs</b><div>&nbsp;</div>
                         <BasicTabs />
                     </Grid>
-                    <Grid item xs={3}>
+                    <Grid item xs={4}>
                         <b>FullWidthTabs</b><div>&nbsp;</div>
                         <FullWidthTabs />
+                    </Grid>
+                    <Grid item xs={4}>
+                        <b>ScrollableTabsButtonAuto</b><div>&nbsp;</div>
+                        <ScrollableTabsButtonAuto />
                     </Grid>
                 </Grid>
             </div>
