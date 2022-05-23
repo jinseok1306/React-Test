413f0ff2164488a2c8b500af4a889c0255244118
jinseok.park <jinseok1306@seetec.com>
2022-05-23 14:22:21 +0900

    MUI DenseMenu 테스트 완료

--- ReactProject/client_app/src/pages/MUI/MenuPage.tsx
+++ ReactProject/client_app/src/pages/MUI/MenuPage.tsx
@@ -3,6 +3,7 @@ import Menu from '../../components/Menu';
 import Grid from '@mui/material/Grid';
 import BasicMenu from '../../components/Menu/BasicMenu';
 import IconMenu from '../../components/Menu/IconMenu';
+import DenseMenu from '../../components/Menu/DenseMenu';
 
 const MenuPage = () => {
     return (
@@ -17,6 +18,10 @@ const MenuPage = () => {
                     <Grid item xs={3}>
                         <b>IconMenu</b><div>&nbsp;</div>
                         <IconMenu />
+                    </Grid>
+                    <Grid item xs={3}>
+                        <b>DenseMenu</b><div>&nbsp;</div>
+                        <DenseMenu />
                     </Grid>
                 </Grid>
             </div>
