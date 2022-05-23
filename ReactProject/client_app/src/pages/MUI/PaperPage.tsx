00ea9df4f0509361e0f69c39ef138ed93d8ba1bd
jinseok.park <jinseok1306@seetec.com>
2022-05-23 10:58:21 +0900

    MUI Elevation 테스트 완료

--- ReactProject/client_app/src/pages/MUI/PaperPage.tsx
+++ ReactProject/client_app/src/pages/MUI/PaperPage.tsx
@@ -3,6 +3,7 @@ import Menu from '../../components/Menu';
 import Grid from '@mui/material/Grid';
 import SimplePaper from '../../components/Paper/SimplePaper';
 import Variants from '../../components/Paper/Variants';
+import Elevation from '../../components/Paper/Elevation';
 
 const PaperPage = () => {
     return (
@@ -17,6 +18,10 @@ const PaperPage = () => {
                     <Grid item xs={3}>
                         <b>Variants</b><div>&nbsp;</div>
                         <Variants />
+                    </Grid>
+                    <Grid item xs={6}>
+                        <b>Elevation</b><div>&nbsp;</div>
+                        <Elevation />
                     </Grid>
                 </Grid>
             </div>
