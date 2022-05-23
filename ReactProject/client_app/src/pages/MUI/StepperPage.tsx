eac0105ac594939e0bd9ede342ecc03670c17585
jinseok.park <jinseok1306@seetec.com>
2022-05-23 14:45:46 +0900

    MUI HorizontalLabelPositionBelowStepper 테스트 완료

--- ReactProject/client_app/src/pages/MUI/StepperPage.tsx
+++ ReactProject/client_app/src/pages/MUI/StepperPage.tsx
@@ -3,6 +3,7 @@ import Menu from '../../components/Menu';
 import Grid from '@mui/material/Grid';
 import HorizontalLinearStepper from '../../components/Stepper/HorizontalLinearStepper';
 import HorizontalNonLinearStepper from '../../components/Stepper/HorizontalNonLinearStepper';
+import HorizontalLabelPositionBelowStepper from '../../components/Stepper/HorizontalLabelPositionBelowStepper';
 
 const StepperPage = () => {
     return (
@@ -17,6 +18,10 @@ const StepperPage = () => {
                     <Grid item xs={4}>
                         <b>HorizontalNonLinearStepper</b><div>&nbsp;</div>
                         <HorizontalNonLinearStepper />
+                    </Grid>
+                    <Grid item xs={4}>
+                        <b>HorizontalLabelPositionBelowStepper</b><div>&nbsp;</div>
+                        <HorizontalLabelPositionBelowStepper />
                     </Grid>
                 </Grid>
             </div>
