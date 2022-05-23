db1c61b786de7831a0372fd889492faaa5721b34
jinseok.park <jinseok1306@seetec.com>
2022-05-23 11:15:26 +0900

    MUI FixedBottomNavigation 테스트 완료

--- ReactProject/client_app/src/pages/MUI/ButtonNavigationPage.tsx
+++ ReactProject/client_app/src/pages/MUI/ButtonNavigationPage.tsx
@@ -3,6 +3,7 @@ import Menu from '../../components/Menu';
 import Grid from '@mui/material/Grid';
 import SimpleBottomNavigation from '../../components/ButtonNavigation/SimpleBottomNavigation';
 import LabelBottomNavigation from '../../components/ButtonNavigation/LabelBottomNavigation';
+import FixedBottomNavigation from '../../components/ButtonNavigation/FixedBottomNavigation';
 
 const ButtonNavigationPage = () => {
     return (
@@ -17,6 +18,10 @@ const ButtonNavigationPage = () => {
                     <Grid item xs={3}>
                         <b>LabelBottomNavigation</b><div>&nbsp;</div>
                         <LabelBottomNavigation />
+                    </Grid>
+                    <Grid item xs={3}>
+                        <b>FixedBottomNavigation</b><div>&nbsp;</div>
+                        <FixedBottomNavigation />
                     </Grid>
                 </Grid>
             </div>
